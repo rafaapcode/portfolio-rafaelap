73c50cae6d35f42b970957ef7f9b4dfb04024e55
rafaapcode <rafaap2003@gmail.com>
2022-11-22 19:49:45 -0300

    Fix the headers

--- portfolio/src/components/Header/Header.jsx
+++ portfolio/src/components/Header/Header.jsx
@@ -3,11 +3,6 @@ import { motion } from 'framer-motion';
 
 function Header() {
 
-  const headerAnimation = {
-    initial: { y: -100 },
-    animate: { y: 0 }
-  }
-
   return (
     <header>
       <div className='hidden md:flex max-w-2xl xl:max-w-5xl mx-auto py-5 justify-between'>
@@ -15,29 +10,17 @@ function Header() {
 
         <div>
           <ul className='flex gap-x-8'>
-            <motion.li
-              {...headerAnimation}
-              transition={{ duration: 0.7, type: 'spring' }}><a href="#home">Home</a></motion.li>
+            <motion.li><a href="#home">Home</a></motion.li>
 
-            <motion.li
-              {...headerAnimation}
-              transition={{ duration: 1.1, type: 'spring' }}><a href="#about">Sobre</a></motion.li>
+            <motion.li><a href="#about">Sobre</a></motion.li>
 
-            <motion.li
-              {...headerAnimation}
-              transition={{ duration: 1.5, type: 'spring' }}><a href="#skills">Skills</a></motion.li>
+            <motion.li><a href="#skills">Skills</a></motion.li>
 
-            <motion.li
-              {...headerAnimation}
-              transition={{ duration: 1.9, type: 'spring' }}><a href="#qualification">Qualificação</a></motion.li>
+            <motion.li><a href="#qualification">Qualificação</a></motion.li>
 
-            <motion.li
-              {...headerAnimation}
-              transition={{ duration: 2.3, type: 'spring' }}><a href="#portfolio">Portfolio</a></motion.li>
+            <motion.li><a href="#portfolio">Portfolio</a></motion.li>
 
-            <motion.li
-              {...headerAnimation}
-              transition={{ duration: 2.8, type: 'spring' }}><a href="#contact">Contato</a></motion.li>
+            <motion.li><a href="#contact">Contato</a></motion.li>
 
           </ul>
         </div>
